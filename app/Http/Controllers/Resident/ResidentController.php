986d3e659ba7f527145f007d9273a485812c06de
mjmarch33333 <marchmikej@gmail.com>
2016-07-27 14:02:23 -0400

    Added resident controller

---
+++ app/Http/Controllers/Resident/ResidentController.php
@@ -0,0 +1,41 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Auth\AuthManager as Auth;
+use Illuminate\Http\Request;
+use App\Events\DeviceUpdateMaintenance;
+use TenantSync\Models\Device;
+use TenantSync\Models\OverdueUsage;
+use TenantSync\Models\OverdueType;
+use TenantSync\Models\Property;
+use TenantSync\Models\User;
+use TenantSync\Mutators\PropertyMutator;
+use Response;
+
+class HomeController extends Controller {
+
+	public function index(Auth $auth)
+	{
+		if($auth->check())
+		{
+			switch($auth->user()->role)
+			{
+				case 'admin':
+					return $this->admin();
+				case 'sales':
+					return $this->sales();
+				case 'landlord':
+					return $this->landlord();
+				case 'manager':
+					return $this->manager();
+			}
+		}
+		return view('auth/login');
+	}
+
+	public function test()
+    {
+    	return "test";
+    }
+
+}  
